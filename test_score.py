b11de35e6f206ee145ebf2c42ae3b88181357d02
Adam J. <106862447+unixthug@users.noreply.github.com>
2026-04-11 20:46:19 -0500

    fixed api check logic bug

--- test_score.py
+++ test_score.py
@@ -12,6 +12,7 @@ def check_health(url):
             response = requests.post(f"{api_url}/score", json={"url": url})
             if response.status_code == 200:
                 print(f"RiskLens is up and running!")
+                pass
             else:
                 print(f"RiskLens is down. Status code: {response.status_code}")
         except requests.exceptions.RequestException as e:
